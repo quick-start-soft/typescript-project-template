05f18c588cab9916ffee270a49cff1920dd5c68a
zeallat <zeallat94@gmail.com>
2024-07-14 19:39:34 +0900

    build: add ESLint configuration file for TypeScript

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    // Customize your rules here
+  },
+};
